0a181b36c70082506c62eace05420b90dfb8273a
GeorgeOprian <49019556+GeorgeOprian@users.noreply.github.com>
2023-03-11 11:54:52 +0200

    connect to db

--- ReadItApp/src/main/java/model/Book.java
+++ ReadItApp/src/main/java/model/Book.java
@@ -4,21 +4,33 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 import lombok.ToString;
 
-@Data
 @ToString
 @Entity
-@Table(name = "BOOK")
+@Table(name = "book")
 public class Book {
 
 	@Id
-	@Column(name = "ISBN")
+	@Column(name = "isbn")
 	private Long isbn;
 
 	@Column(name = "name")
 	private String name;
 
+	public Long getIsbn() {
+		return isbn;
+	}
 
+	public void setIsbn(Long isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
