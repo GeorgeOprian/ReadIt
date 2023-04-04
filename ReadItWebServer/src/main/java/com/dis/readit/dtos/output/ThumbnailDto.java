723d71ffb48088dee14dc26e930cc9d8fd3842c1
GeorgeOprian <49019556+GeorgeOprian@users.noreply.github.com>
2023-04-04 22:11:54 +0300

    rename

--- ReadItWebServer/src/main/java/com/dis/readit/dtos/output/ThumbnailDto.java
+++ ReadItWebServer/src/main/java/com/dis/readit/dtos/output/ThumbnailDto.java
@@ -1,13 +1,12 @@
 package com.dis.readit.dtos.output;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
-public class ThumbNailDto implements Serializable {
+public class ThumbnailDto implements Serializable {
 	@JsonProperty("smallThumbnail")
 	private String smallThumbnail;
 
