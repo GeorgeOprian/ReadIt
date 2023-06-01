c8421c8636c1acfabc267dac09cfff087421597c
GeorgeOprian <49019556+GeorgeOprian@users.noreply.github.com>
2023-06-01 23:28:32 +0300

    Replace LocalDate with String

--- ReadItApp/app/src/main/java/com/example/readitapp/model/webserver/SubscriptionDto.java
+++ ReadItApp/app/src/main/java/com/example/readitapp/model/webserver/SubscriptionDto.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.time.LocalDate;
+
 
 public class SubscriptionDto {
 
@@ -16,15 +16,51 @@ public class SubscriptionDto {
     @NotNull
     @SerializedName("startDate")
     @Expose
-    private LocalDate startDate;
+    private String startDate;
 
     @NotNull
     @SerializedName("endDate")
     @Expose
-    private LocalDate endDate;
+    private String endDate;
 
     @NotNull
     @SerializedName("userEmail")
     @Expose
     private String userEmail;
+
+
+    public Integer getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    public void setSubscriptionId(Integer subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
+
+    @NotNull
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(@NotNull String startDate) {
+        this.startDate = startDate;
+    }
+
+    @NotNull
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(@NotNull String endDate) {
+        this.endDate = endDate;
+    }
+
+    @NotNull
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(@NotNull String userEmail) {
+        this.userEmail = userEmail;
+    }
 }
